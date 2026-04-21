510be1357e1c9a73de83b30df98a13cd91a06970
DragoQC <alexisgauthier2001@hotmail.com>
2026-04-21 14:57:51 -0400

    Fix invite response...

--- serverwebapp/Contracts/Api/Admin/InviteRemoteServerRequest.cs
+++ serverwebapp/Contracts/Api/Admin/InviteRemoteServerRequest.cs
@@ -1,10 +1,9 @@
 namespace AsaServerManager.Web.Contracts.Api.Admin;
 
 public sealed record InviteRemoteServerRequest(
-    string VpnAddress,
     string ClusterId,
+    string VpnAddress,
     string ServerEndpoint,
-    string Dns,
     string AllowedIps,
     string RemoteApiKey,
     string ServerPublicKey,
